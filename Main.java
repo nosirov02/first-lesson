578737d987f8dac450add4b7c913654666dd6bef
nosirov02 <n.samandar1502@gmail.com>
2023-01-07 20:03:37 +0500

    init main project

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println(sum(5,7));
+    }
+
+
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+}
